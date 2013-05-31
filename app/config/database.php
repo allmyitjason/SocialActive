2c960eb4cec2410a6a50535f473fcb577dfdf665
Jason Smith <jason@allmyit.com.au>
2013-06-01 02:15:17 +0930

    - Updated config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'sport',
+			'username'  => 'sport',
+			'password'  => '43twtwret',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
